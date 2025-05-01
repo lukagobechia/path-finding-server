63041c87170a24cb607c22dea109e382d4100cf5
Luka Gobechia <114133823+lukagobechia@users.noreply.github.com>
2025-05-02 03:24:54 +0400

    Update app.py

--- pathfinder_api/app.py
+++ pathfinder_api/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from pathfinder_api.routes.pathfinder import pathfinder_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(pathfinder_bp)
 
 @app.route('/')
